86fdf0059a93dc23ab499ec682851b2ed37c942b
maddockj1 <jmmaddock@gmail.com>
2018-11-26 13:58:02 -0700

    organized

---
+++ src/components/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../header/header'
+import Footer from '../footer/footer'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <Header/>
+      
+      
+
+      <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
