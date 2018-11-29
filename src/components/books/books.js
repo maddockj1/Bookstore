b5d9a1411787e79c9e6d8ae08ae1e7adc555c0be
maddockj1 <jmmaddock@gmail.com>
2018-11-29 15:23:23 -0700

    spruced

--- src/components/books/books.js
+++ src/components/books/books.js
@@ -12,7 +12,7 @@ const Books = ({ books, addToCart }) => {
       <div className="col-md-6">
         <div>{books.title}</div>
         <div>{books.subtitle}</div>
-        <div>{books.author}</div>
+        <div>Author: {books.author}</div>
         <div>${books.price}.00</div>
         <br />
         <button onClick={addItem} value={books.title} id={books.id}>
